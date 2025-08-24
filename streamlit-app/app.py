5637f3b7c7726a4c3762b01f2eb103b94929d8e5
raviaddakula61 <134484614+raviaddakula61@users.noreply.github.com>
2025-08-24 22:58:06 +0530

    Update app.py

--- streamlit-app/app.py
+++ streamlit-app/app.py
@@ -4,7 +4,7 @@ import streamlit as st
 import helper
 
 # Load model
-model_path = os.path.join(os.path.dirname(__file__), "model_quora.pkl")
+model_path = os.path.join(os.path.dirname(__file__), "model.pkl")
 with open(model_path, "rb") as f:
     model = pickle.load(f)
 
